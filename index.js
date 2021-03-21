d4b42ac2637d711c2d3e4321b9b23e025b33f950
Nitesh Raut <rautesh6@gmail.com>
2021-03-21 17:24:03 +0545

    [Fixed] hero section done

---
+++ index.js
@@ -0,0 +1,23 @@
+const header = document.querySelector("header");
+window.addEventListener("scroll", () => {
+  header.classList.toggle("scrolled-nav", window.scrollY > 250);
+});
+
+const job = document.querySelector(".change-text");
+
+job_list = [
+  "Web Developer",
+  "Web Designer",
+  "UI/UX Developer",
+  "Frontend Developer",
+  "Student",
+];
+a = 0;
+setInterval(() => {
+  job.innerHTML = job_list[a];
+  a++;
+  console.log(a);
+  if (a >= job_list.length) {
+    a = 0;
+  }
+}, 1000);
